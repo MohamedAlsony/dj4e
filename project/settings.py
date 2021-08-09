8b7ef3b5091f5533839884029ba545ac7ecb260a
MohamedAlsony <mohamedahmedelsony@gmail.com>
2021-08-09 16:53:47 +0200

    '#'

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c7yi+2ks3p9##wnk^vm578b0!r(#pz=s%qstw9r7223@atvo5#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
