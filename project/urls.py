a61b89ccbd3a0bcb7c952e6bfc6470f9105637f9
MohamedAlsony <mohamedahmedelsony@gmail.com>
2021-08-10 16:08:26 +0200

    '#'

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls', namespace='polls')),
+    path('', include('hello.urls', namespace='hello')),
 ]
